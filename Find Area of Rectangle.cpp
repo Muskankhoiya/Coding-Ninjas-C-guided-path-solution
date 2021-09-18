7265412d48ee036aef0402f6cc266a6cb6ed613d
muskan khoiya <65762360+Muskankhoiya@users.noreply.github.com>
2021-09-18 17:07:20 +0530

    Create Find Area of Rectangle.cpp

---
+++ Find Area of Rectangle.cpp
@@ -0,0 +1,19 @@
+/*Problem Statement
+Take the length(L) and breadth(B) of the rectangle as input and find its area.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+int length, breadth;
+    int area;
+    
+    cin>>length;
+    cin>>breadth;
+    area= length*breadth;
+    cout<<area;
+    
+    
+    return 0;
+}
